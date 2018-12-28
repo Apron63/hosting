c3e3b71007e0f3e133054e0346ed29ff464c26f0
apron63 <apron63@yandex.ru>
2018-12-28 23:29:52 +0400

    init

---
+++ frontend/views/layouts/main.php
@@ -0,0 +1,73 @@
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Breadcrumbs;
+use frontend\assets\AppAsset;
+use common\widgets\Alert;
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <?php $this->head() ?>
+</head>
+<body>
+<?php $this->beginBody() ?>
+
+<div class="wrap">
+    <?php
+    NavBar::begin([
+        'brandLabel' => Yii::$app->name,
+        'brandUrl' => Yii::$app->homeUrl,
+        'options' => [
+            'class' => 'navbar-inverse navbar-fixed-top',
+        ],
+    ]);
+    $menuItems = [
+        ['label' => 'Статьи', 'url' => ['/site/public']],
+        ['label' => 'Заметки', 'url' => ['/site/note']],
+        ['label' => 'Проекты', 'url' => ['/site/project']],
+		['label' => 'Про меня', 'url' => ['/site/about-me']],
+    ];
+    
+    echo Nav::widget([
+        'options' => ['class' => 'navbar-nav navbar-right'],
+        'items' => $menuItems,
+    ]);
+    NavBar::end();
+    ?>
+
+    <div class="container">
+        <?= Breadcrumbs::widget([
+            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+        ]) ?>
+        <?= Alert::widget() ?>
+        <?= $content ?>
+    </div>
+</div>
+
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
+
+        <p class="pull-right"><?= Yii::powered() ?></p>
+    </div>
+</footer>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
---
+++ frontend/views/site/index.php
@@ -0,0 +1,35 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Истоки';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Здравствуйте!</h1>
+
+        <p class="lead">Спасибо, что заглянули в гости на чашечку кофе )</p>
+
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+			<center><h2>Вступление</h2></center>
+            <div class="col-lg-6">
+				<img src="images/elephant.jpg" height="300">
+			</div>
+
+			<div class="col-lg-6">
+				Это слон. Он символ языка PHP, который я использую для своих работ. Конечно, одним PHP невозможно ограничиться,
+				тем не менее он составляет основу моих проектов.
+			</div>
+		</div>
+
+		<div class="row">
+			<center><h2>Мои проекты</h2></center>
+		</div>
+
+    </div>
+</div>
